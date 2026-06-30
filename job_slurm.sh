c8adc73ee613d408871c581ed920d5d42295ca04
RWillat <rwillat@anl.gov>
2026-06-30 12:02:05 -0500

    updated job script

--- job_slurm.sh
+++ job_slurm.sh
@@ -22,6 +22,7 @@ module load griffin-openmpi
 # mpirun -n 12 griffin-opt -i PNSource_test_RZ.i > run.log 2&1
 mpiexec=/apps/local/openmpi/5.0.5-gcc13.2.0-container/bin/mpirun
 griffinexec=/hpc-common/moose/containers/griffin-openmpi/2026.02.27-a7e65e5_ee1118e844308a213b63dfac1474f31ad5597d1e10d92be98e22aa8724c96338/bin/griffin-opt
-inputfile=GriffinTests/HPMR_OneSixth_Core_meshgenerator_tri.i
+#inputfile=GriffinTests/HPMR_OneSixth_Core_meshgenerator_tri.i
+inputfile=GriffinTests/NEP_HPMR_meshgenerator_tri.i
 
 $mpiexec -n 20 $griffinexec -i $inputfile # > run.log 2&1
